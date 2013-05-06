a8a3582a00a250fb32f0402a2900b434df62e13b
bmg74 <bmg74@drexel.edu>
2013-05-06 11:08:56 -0400

    Put the MAX_CLUSTER constant back to 10

--- src/Location.java
+++ src/Location.java
@@ -4,7 +4,7 @@ public class Location
 {
 	public static Random rand = new Random();
 	public static FileOps Ops = new FileOps();
-	public static int MAX_CLUSTERS = 50;
+	public static int MAX_CLUSTERS = 10;
 	public static int MAX_SECTORS = 10;
 	public static int MAX_SYSTEMS = 10;
 	public static int MAX_PLANETS = 10;
