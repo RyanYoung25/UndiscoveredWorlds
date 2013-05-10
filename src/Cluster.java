ca3e30301597851054fb208c021e5ae779b2f526
bmg74 <bmg74@drexel.edu>
2013-05-09 21:57:48 -0400

    Added comments to Cluster and Location

--- src/Cluster.java
+++ src/Cluster.java
@@ -1,15 +1,23 @@
 import java.awt.image.BufferedImage;
 
+/**
+ * The Cluster class provides the next structural tier after the Galaxy class for galaxy generation and manipulation.
+ * @author Bryant
+ */
 public class Cluster extends Location
 {
-	private Sector[] Sectors;
-	private long seed;
-	private short Name;
-	private short Name2;
-	private int x;
-	private int y;
+	private Sector[] Sectors;	// Holds all sector information for this cluster.
+	private long seed;			// Seed value provided by Galaxy.
+	private short Name;			// Number corresponding to entry in Ops.ClusterNames.
+	private short Name2;		// Number corresponding to entry in Ops.ClusterNames2.
+	private int x;				// X coordinate data.
+	private int y;				// Y coordinate data.
 	
 
+	/**
+	 * Constructor.
+	 * @param s = seed value provided by Galaxy.
+	 */
 	public Cluster(long s)
 	{
 		super();
@@ -19,6 +27,13 @@ public class Cluster extends Location
 		Picture = Pics.getClusterPic(rand.nextInt(10));
 	}
 	
+	/**
+	 * Constructor.
+	 * @param s = seed value provided by Galaxy.
+	 * @param name = number corresponding to entry in Ops.ClusterNames.
+	 * @param name2 = number corresponding to entry in Ops.ClusterNames2.
+	 * @param pic = assigned BufferedImage.
+	 */
 	public Cluster(long s, short name, short name2, BufferedImage pic)
 	{
 		super();
@@ -29,6 +44,9 @@ public class Cluster extends Location
 		Generate();
 	}
 	
+	/**
+	 * Handles generation of all sectors within this cluster.
+	 */
 	public void Generate()
 	{
 		Sectors = new Sector[1 + rand.nextInt(MAX_SECTORS)];
@@ -42,52 +60,90 @@ public class Cluster extends Location
 		}
 	}
 	
+	// Returns cluster name in string format
 	@Override
 	public String toString()
 	{
 		return Ops.getCluster(Name, Name2);
 	}
 	
+	/**
+	 * Returns a specific sector in this cluster.
+	 * @param index = index of a particular sector within this cluster's sector array.
+	 * @return Sector
+	 */
 	public Sector GetSector(int index)
 	{
 		return GetSector()[index];
 	}
 	
+	/**
+	 * Returns all of the sectors in this cluster.
+	 * @return Sector[]
+	 */
 	public Sector[] GetSector()
 	{
 		return Sectors;
 	}
 	
+	/**
+	 * Returns seed value of this cluster.
+	 * @return long
+	 */
 	public long GetSeed()
 	{
 		return seed;
 	}
 	
+	/**
+	 * Returns number that is used to obtain the first part of this cluster's name
+	 * @return short
+	 */
 	public short GetName()
 	{
 		return Name;
 	}
 	
+	/**
+	 * Returns number that is used to obtain the second part of this cluster's name
+	 * @return short
+	 */
 	public short GetName2()
 	{
 		return Name2;
 	}
 	
+	/**
+	 * Gets this cluster's X coordinate within its galaxy.
+	 * @return int
+	 */
 	public int GetX()
 	{
 		return x;
 	}
 	
+	/**
+	 * Gets this cluster's Y coordinate within its galaxy.
+	 * @return int
+	 */
 	public int GetY()
 	{
 		return y;
 	}
 	
+	/**
+	 * Sets this cluster's X coordinate within its galaxy.
+	 * @param X
+	 */
 	public void SetX(int X)
 	{
 		x = X;
 	}
 	
+	/**
+	 * Sets this cluster's Y coordinate within its galaxy.
+	 * @param Y
+	 */
 	public void SetY(int Y)
 	{
 		y = Y;
--- src/Location.java
+++ src/Location.java
@@ -1,25 +1,36 @@
 import java.awt.image.BufferedImage;
 import java.util.Random;
 
+/**
+ * Parent class for all locations.
+ * @author Bryant
+ */
 public class Location 
 {
-	public static Random rand = new Random();
-	public static FileOps Ops = new FileOps();
-	public static PictureAlbum Pics = new PictureAlbum();
+	public static Random rand = new Random();				// Static Random object for use in all child classes.
+	public static FileOps Ops = new FileOps();				// Static FileOps object for use in all child classes.
+	public static PictureAlbum Pics = new PictureAlbum();	// Static PictureAlbum object for use in all child classes.
 	
-	public static int MAX_CLUSTERS = 10;
-	public static int MAX_SECTORS = 10;
-	public static int MAX_SYSTEMS = 10;
-	public static int MAX_PLANETS = 10;
-	public static int MAX_MOONS = 10;
+	public static int MAX_CLUSTERS = 10;					// Constant determines number of clusters in a galaxy.
+	public static int MAX_SECTORS = 10;						// Constant determines number of sectors in cluster (1 to MAX)
+	public static int MAX_SYSTEMS = 10;						// Constant determines number of stars in sector (1 to MAX)
+	public static int MAX_PLANETS = 10;						// Constant determines number of orbitals around a star (1 to MAX)
+	public static int MAX_MOONS = 10;						// Constant determines number of orbitals around a planet (1 to MAX)
 	
-	protected BufferedImage Picture;
+	protected BufferedImage Picture;						// Used to reference image data
 	
+	/**
+	 * constructor
+	 */
 	public Location()
 	{
-		
+		//does nothing
 	}
 	
+	/**
+	 * Returns location's image data
+	 * @return
+	 */
 	public BufferedImage GetPic()
 	{
 		return Picture;
