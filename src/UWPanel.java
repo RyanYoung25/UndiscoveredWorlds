2bdc67538529b76849da2e8863112dbfe6231e0a
Ryan Young <rdy29@drexel.edu>
2013-05-13 15:54:53 -0400

    The image file separator was added

--- src/UWPanel.java
+++ src/UWPanel.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.io.File;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
@@ -16,9 +17,9 @@ public class UWPanel extends JPanel{
 		setPreferredSize(new Dimension(720,700));
 		setBackground(Color.GREEN);
 		
-		label = new JLabel();
-		label.setToolTipText("" + new ImageIcon("Art/Barren01.png").getDescription());
-		label.setIcon(new ImageIcon("Art/Barren01.png"));
+		label = new JLabel("hi");
+		label.setToolTipText("" + new ImageIcon("Art" + File.separator + "Barren01.png").getDescription());
+		label.setIcon(new ImageIcon("Art" + File.separator + "Barren01.png"));
 		add(label);
 	}
 }
