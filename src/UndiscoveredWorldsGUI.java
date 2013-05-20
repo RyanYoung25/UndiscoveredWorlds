33427d647c790e9bb8eddf9b57f9826368bc386e
Jonathan McDaniel <jonmd24@gmail.com>
2013-05-20 13:59:43 -0400

    Partial Commit on start screen

--- src/UndiscoveredWorldsGUI.java
+++ src/UndiscoveredWorldsGUI.java
@@ -8,10 +8,48 @@ import javax.swing.*;
 public class UndiscoveredWorldsGUI extends JFrame implements ActionListener {
 
 	private JButton optionsButton;
+	private JPanel startPanel;
+	private JPanel storyPanel;
+	private JLabel logoLabel;
+	private JButton newGameButton;
+	private JButton loadGameButton;
 
 	public UndiscoveredWorldsGUI() {
 		super("Undiscovered Worlds");
 		setBackground(Color.BLACK);
+		setLayout(null);
+		
+		logoLabel = new JLabel(new ImageIcon("Art" + File.separator + "LogoTitle.png"));
+		logoLabel.setSize(700, 200);
+		logoLabel.setLocation(
+								(int) (370 - (logoLabel.getWidth()/2)),
+								(int) (70));
+		newGameButton = new JButton("New Game");
+		newGameButton.setSize(400, 100);
+		newGameButton.setLocation(
+								(int) (370 - (newGameButton.getWidth()/2)),
+								(int) ((370 - (newGameButton.getHeight()/2) + 50)));
+		newGameButton.addActionListener(this);
+		
+		loadGameButton = new JButton("Load Game");
+		loadGameButton.setSize(400, 100);
+		loadGameButton.setLocation(
+								(int) (newGameButton.getLocation().getX()),
+								(int) ((newGameButton.getLocation().getY() + 120)));
+		loadGameButton.addActionListener(this);
+		
+		startPanel = new JPanel();
+		startPanel.setLayout(null);
+		startPanel.setSize(740, 740);
+		startPanel.setLocation(0, 0);
+		startPanel.setVisible(true);
+		startPanel.setBackground(Color.DARK_GRAY);
+		
+		startPanel.add(logoLabel);
+		startPanel.add(newGameButton);
+		startPanel.add(loadGameButton);
+		
+		//add(startPanel);
 		
 		/*
 		optionsButton = new JButton();
@@ -31,11 +69,12 @@ public class UndiscoveredWorldsGUI extends JFrame implements ActionListener {
 		player.setLoc(Andromeda.randomStart());
 
 		SpacePanel test = new SpacePanel(player);
+		test.setVisible(true);
 		// test.populateSpace();
 		// test.setPreferredSize(new Dimension(740, 740));
 		// test.setLocation(0, 0);
 		//this.add(optionsButton);
-		this.add(test);
+		add(test);
 		// System.out.println("This was run");
 		/*
 		 * GUITest test = new GUITest(Andromeda.GetSectors());
